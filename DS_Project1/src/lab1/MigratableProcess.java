decc33c75768f94d9bf97ea5d80bcb7b48470c3a
zjlxzc <zjlxz@hotmail.com>
2014-09-02 13:44:33 -0400

    test

---
+++ DS_Project1/src/lab1/MigratableProcess.java
@@ -0,0 +1,15 @@
+package lab1;
+import java.io.Serializable;
+
+/* The interface extends Runnable to be run via a Thread object.
+ * The interface also extends Serializable to be serialized and written to or read from a stream.
+*/
+
+public interface MigratableProcess extends Runnable, Serializable {
+	/* This method will be called before the object is serialized.
+	 * It affords an opportunity for the process to enter a known safe state.
+	 */
+	
+	public String toString();
+	void suspend();
+}
