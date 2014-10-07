682cfec99afdefe688c71f48ca35d3aef58a2f91
chunx <chunx@andrew.cmu.edu>
2014-10-07 19:31:15 -0400

    qq

--- DS_Project2/src/textScraper/TextScraper.java
+++ DS_Project2/src/textScraper/TextScraper.java
@@ -6,22 +6,16 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 
 public class TextScraper {
-
-	private String totalNumber;
-	private ArrayList<ResultObject> resultObjects;
 	
 	public TextScraper() {
-		resultObjects = new ArrayList<ResultObject>();
 	}
 	
 	public static void main(String[] args) throws IOException {
 		TextScraper textScraper = new TextScraper();
 		
 		String keyword = args[0];
-		int pageNumber = 0;
 		
 		String[] words = keyword.split(" ");
 		StringBuilder key = new StringBuilder();
@@ -44,76 +38,20 @@ public class TextScraper {
         }
         in.close();
         
-		if (args.length == 1) {
-			textScraper.query1(response, keyword);
-		} else {
-			pageNumber = Integer.parseInt(args[1]);
-			textScraper.query2(response, keyword, pageNumber);
-		}
-		
-		textScraper.printObjects();
+		textScraper.query1(response, keyword);
 	}
 	
 	public void query1(String response, String keyword) throws UnsupportedEncodingException {
+		String totalNumber = "0";
 		if(response.indexOf("numTotalResults") != -1){
 			int index = response.indexOf("numTotalResults");
             int startIndex = response.indexOf("of", index);
             int endIndex = response.indexOf("</span>", index);
            
             totalNumber = response.substring(startIndex + 3, endIndex); 
-            System.out.println("totalNumber: " + totalNumber);
         }
-        else {
-            totalNumber = "0";
-        }
-	}
-	
-	public void query2(String response, String keyword, int pageNumber) {
-		if(response.indexOf("selected" + pageNumber) != -1){
-			ResultObject tempObject = new ResultObject();
-			int signalIndex = 0;
-			
-			while (true) {
-				int nameIndex = response.indexOf("productName", signalIndex);
-				if (nameIndex == -1) {
-					break;
-				}
-				
-				tempObject = new ResultObject();
-				int titleIndex = response.indexOf("title", nameIndex);
-				int startIndex = response.indexOf("\"", titleIndex);
-				int endIndex = response.indexOf("\">", startIndex);
-				tempObject.setTitle(response.substring(startIndex + 1, endIndex));
-				
-				int priceIndex = response.indexOf("productPrice", nameIndex);
-				startIndex = response.indexOf("$", priceIndex);
-				endIndex = response.indexOf("</", startIndex);
-				tempObject.setProductPrice(response.substring(startIndex, endIndex));
-				
-				int shipIndex = (response.indexOf("freeShip", nameIndex) != -1) ?
-						(response.indexOf("freeShip", nameIndex)) : (response.indexOf("calc", nameIndex));
-				startIndex = response.indexOf(">", shipIndex);
-				endIndex = response.indexOf("</", startIndex);
-				tempObject.setShippingPrice(response.substring(startIndex + 1, endIndex));
-				
-				int merchantIndex = response.indexOf("newMerchantName", nameIndex);
-				startIndex = response.indexOf(">", merchantIndex);
-				endIndex = response.indexOf("</", startIndex);
-				tempObject.setVendor(response.substring(startIndex + 1, endIndex));
-				
-				signalIndex = endIndex;
-				resultObjects.add(tempObject);
-			}
-        }
-        else {
-        	resultObjects = new ArrayList<ResultObject>();
-        }
-	}
-	
-	public void printObjects() {
-		for (ResultObject object : resultObjects) {
-			System.out.println(object.toString());
-		}
+		
+		System.out.println("totalNumber: " + totalNumber);
 	}
 }
 
