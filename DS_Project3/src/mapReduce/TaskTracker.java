4b9faa613f7d693ca9e4b59ad04787fba1e137e1
chunx <chunx@andrew.cmu.edu>
2014-11-09 16:13:29 -0500

    kk

--- DS_Project3/src/mapReduce/TaskTracker.java
+++ DS_Project3/src/mapReduce/TaskTracker.java
@@ -1,7 +1,10 @@
 package mapReduce;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -16,26 +19,28 @@ import fileio.FileWriterDFS;
 
 public class TaskTracker {
 
-	private String hostname;
-	private Class<?> MRClass;
-	private String outputPath;
-	private ArrayList<BlockRef> blocks;
-	private ArrayList<NodeRef> reducers;
+	private static TaskTracker instance;
 	
-	private String mapRed = "";	
-	private boolean isFinished = false;
-	private int recordCount = 10;
+	private Class<?> MRClass;
+	private static Hashtable<Integer, String> status;
+	
+	private static boolean isFinished = false;
+	private static int recordCount = 10;
 	
 	//private Thread reportThread;
 	private int reportPort = 15640;
 	
-	public TaskTracker(Task task) throws IOException {
-		//hostname = ;
-		//MRClass = ;
-		//outputPath = ;
-		//blocks = ;
-		//reducers = ;
-
+	public static TaskTracker getInstance() {
+		if (instance == null) {
+			instance = new TaskTracker();
+		}
+		return instance;
+	}
+	
+	public TaskTracker() {
+	}
+	
+	public void add(Task task) throws IOException {
 		try {
 			ServerSocket taskListenSocket = new ServerSocket(15640);
 			Thread listenThread = new Thread(new Listen(taskListenSocket));		
@@ -48,12 +53,18 @@ public class TaskTracker {
 		}
 	}
 	
-	public void trackMapper(int num) throws FileNotFoundException {
+	public static void trackMapper(MapperTask task) throws FileNotFoundException {
+		int taskID = task.getTaskID();
+		ArrayList<BlockRef> blocks = task.getBlockList();
+		ArrayList<NodeRef> reducers = task.getReducers();
+		int num = task.getBlockList().size();
+		
 		for (int i = 0; i < num; i++) {
 			FileReaderDFS map = new FileReaderDFS(blocks.get(i).getFileName(), reducers);
 			Thread mapper = new Thread(map);
 			mapper.start();
-		
+			status.put(taskID, "Starting");
+			
 			while (true) {
 				if (map.getCount() != recordCount) {
 					isFinished = false;
@@ -62,10 +73,14 @@ public class TaskTracker {
 					break;
 				}
 			}
+			status.put(taskID, "Finished");
 		}
 	}
 	
-	public void trackReducer(int num) throws IOException {
+	public static void trackReducer(ReducerTask task) throws IOException {
+		String outputPath = task.getOutputPath();
+		int taskID = task.getTaskID();
+		int num = task.ge
 		for (int i = 0; i < num; i++) {
 			FileWriterDFS reduce = new FileWriterDFS(outputPath);
 			Thread reducer = new Thread(reduce);
@@ -77,7 +92,7 @@ public class TaskTracker {
 		return isFinished;
 	}
 	
-	private class Listen implements Runnable {
+	private static class Listen implements Runnable {
 		ServerSocket taskListenSocket;
 		public Listen(ServerSocket taskListenSocket) {
 			this.taskListenSocket = taskListenSocket;
@@ -85,14 +100,25 @@ public class TaskTracker {
 		
 		public void run() {
 			while (true) {
-				Socket master;
 				try {
-					master = taskListenSocket.accept();					
-					if (mapRed.equals("mapper")) {
-						trackMapper(blocks.size());
+					Socket nameNode = taskListenSocket.accept();					
+					InputStreamReader inStream = new InputStreamReader(nameNode.getInputStream());
+					BufferedReader br = new BufferedReader(inStream); 
+					PrintWriter outStream = new PrintWriter(nameNode.getOutputStream());				
+					
+					String inLine = br.readLine();
+					if (inLine.equals("MapperTask")) {
+						br = new BufferedReader(inStream); 
+						ObjectInputStream object = new ObjectInputStream(nameNode.getInputStream());
+						MapperTask task = (MapperTask)object.readObject();
+						trackMapper(task);
 					} else {
-						trackReducer(reducers.size());
-					}					
+						br = new BufferedReader(inStream); 
+						ObjectInputStream object = new ObjectInputStream(nameNode.getInputStream());
+						ReducerTask task = (ReducerTask)object.readObject();
+						trackReducer(task);
+					}				
+					
 				} catch (Exception e) {
 					System.out.println(e);
 				}				
